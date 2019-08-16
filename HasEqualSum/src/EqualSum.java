f77c90dd613a7e2eb406ef09af759f500cb7944f
saxenaharsh <harsh.saxena@gmail.com>
2019-08-16 16:27:38 +0530

    Add files via upload

---
+++ HasEqualSum/src/EqualSum.java
@@ -0,0 +1,12 @@
+public class EqualSum {
+
+    public static boolean HasEqualSum(int numOne, int numTwo, int numThree){
+            if(numOne + numTwo  == numThree){
+                System.out.println("true");
+                return true;
+            }else{
+                System.out.println("false");
+                return false;
+            }
+    }
+}
---
+++ HasEqualSum/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        EqualSum.HasEqualSum(1, -1, 3);
+    }
+
+}
